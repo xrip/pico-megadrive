05702c7ae70803b0ca2abf22364a1c632a1a4c93
xrip <xrip@xrip.ru>
2024-02-23 09:42:19 +0300

    soft tv composite initial

--- drivers/graphics/graphics.h
+++ drivers/graphics/graphics.h
@@ -18,7 +18,9 @@ extern "C" {
 #ifdef TV
 #include "tv.h"
 #endif
-
+#ifdef SOFTTV
+#include "tv-software.h"
+#endif
 #include "font6x8.h"
 #include "font8x8.h"
 #include "font8x16.h"
---
+++ drivers/tv-software/tv-software.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "inttypes.h"
+#include "stdbool.h"
+
+#define PIO_VIDEO pio0
+
+#define beginVideo_PIN (6)
+
+#define TEXTMODE_COLS 53
+#define TEXTMODE_ROWS 30
+
+
+
+typedef enum g_mode_t{
+    g_mode_320x240x8bpp,
+    g_mode_320x240x4bpp
+}g_mode_t;
+
+typedef enum g_out_TV_t{
+    g_TV_OUT_PAL,
+    g_TV_OUT_NTSC
+}g_out_TV_t;
+
+
+typedef enum NUM_TV_LINES_t
+{    _624_lines,_625_lines,_524_lines,_525_lines,
+}NUM_TV_LINES_t;
+
+typedef enum COLOR_FREQ_t
+{    _3579545,_4433619
+}COLOR_FREQ_t;
+
+typedef  struct tv_out_mode_t 
+{
+    // double color_freq;
+    float  color_index;
+    COLOR_FREQ_t c_freq;
+    g_mode_t mode_bpp;
+    g_out_TV_t tv_system;
+    NUM_TV_LINES_t N_lines;
+    bool cb_sync_PI_shift_lines;
+    bool cb_sync_PI_shift_half_frame;
+
+    
+}tv_out_mode_t;
+
+
+
+void graphics_set_modeTV(tv_out_mode_t mode);
+tv_out_mode_t graphics_get_default_modeTV();
+
+void graphics_set_paletteTV(uint8_t i, uint32_t color888);
+
+
+//для совместимости(удалить в других проектах)
+typedef enum fr_rate{
+    rate_60Hz = 0,
+    rate_72Hz = 1,
+    rate_75Hz = 2,
+    rate_85Hz = 3
+}fr_rate;
+
+typedef enum g_out{
+    g_out_AUTO  = 0,
+    g_out_VGA   = 1,
+    g_out_HDMI  = 2
+}g_out;
+bool graphics_try_framerate(g_out g_out,fr_rate rate, bool apply);
